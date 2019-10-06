94f6574fdd78b735803441e40653741fc97ba2d9
Mohamed Fathy <codetwostart@gmail.com>
2019-10-06 15:27:42 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace datayptes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22;
+            sbyte b = -128;
+            long i1 = 2212222324;
+            Console.WriteLine(b);
+            char c1 = 'b';
+            Console.WriteLine(c1);
+            Console.WriteLine("Hello World!");
+            string myName = "Mohamed";
+            Console.WriteLine(myName);
+        }
+    }
+}
